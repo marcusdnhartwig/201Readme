3bf8a78a1762fb2498f6aba65c567359f77dd837
Marcus Hartwig <marcusdnhartwig@gmail.com>
2022-02-13 12:06:50 -0800

    just played around

--- Reading Notes/read01.js
+++ Reading Notes/read01.js
@@ -1,15 +1,17 @@
-var today = new Date(); 
-var hourNow = today.getHours();
-var greeting;
+// var today = new Date(); 
+// var hourNow = today.getHours();
+// var greeting;
 
-if (hourNow > 18) {
-    greeting = "Good Evening!";
-} else if (hourNow > 11) {
-    greeting = "Good Afternoon!";
-} else if (hourNow > 0) {
-    greeting = "Good Morning!";
-} else {
-    greeting = "Welcome!";
-}
+// if (hourNow > 18) {
+//     greeting = "Good Evening!";
+// } else if (hourNow > 11) {
+//     greeting = "Good Afternoon!";
+// } else if (hourNow > 0) {
+//     greeting = "Good Morning!";
+// } else {
+//     greeting = "Welcome!";
+// }
 
-document.write('<h3>'+ greeting + '</h2>');
+// document.write('<h3>'+ greeting + '</h2>');
+
+
